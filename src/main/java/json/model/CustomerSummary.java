fd10eb5efcbec0fc21f707057138f587f82964d8
Bayyrem HAMMAMI <hammamibayrem404@gmail.com>
2022-01-01 00:24:11 +0100

    CustomerSummaryServiceTest

--- src/main/java/json/model/CustomerSummary.java
+++ src/main/java/json/model/CustomerSummary.java
@@ -12,4 +12,16 @@ public class CustomerSummary {
         this.totalCostInCents = totalCostInCents;
         this.trips = trips;
     }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public int getTotalCostInCents() {
+        return totalCostInCents;
+    }
+
+    public List<Trip> getTrips() {
+        return trips;
+    }
 }
--- src/test/java/service/CustomerSummaryServiceTest.java
+++ src/test/java/service/CustomerSummaryServiceTest.java
@@ -1,11 +1,17 @@
 package service;
 
+import com.spotify.hamcrest.pojo.IsPojo;
 import json.model.CustomerSummary;
 import json.model.Tap;
+import json.model.Trip;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
@@ -43,6 +49,36 @@ public class CustomerSummaryServiceTest {
 
         List<CustomerSummary> result = customerSummaryService.getCustomerSummaries(tapsByCustomer);
 
+        Assert.assertThat(result.get(0), IsPojo.pojo(CustomerSummary.class)
+                .withProperty("customerId", Matchers.is(1))
+                .withProperty("totalCostInCents",Matchers.is(240)));
+        Assert.assertThat(result.get(0).getTrips().size(), Matchers.is(1));
+        Assert.assertThat(result.get(0).getTrips().get(0), IsPojo.pojo(Trip.class)
+                .where(Trip::getStationStart, Matchers.is("A"))
+                .where(Trip::getStationEnd, Matchers.is("B"))
+                .where(Trip::getStartedJourneyAt, Matchers.is(1L))
+                .where(Trip::getCostInCents, Matchers.is(240))
+                .where(Trip::getZoneFrom, Matchers.is(1))
+                .where(Trip::getZoneTo, Matchers.is(1)));
+
+        Assert.assertThat(result.get(1), IsPojo.pojo(CustomerSummary.class)
+                .withProperty("customerId", Matchers.is(2))
+                .withProperty("totalCostInCents",Matchers.is(440)));
+        Assert.assertThat(result.get(1).getTrips().size(), Matchers.is(2));
+        Assert.assertThat(result.get(1).getTrips().get(0), IsPojo.pojo(Trip.class)
+                .where(Trip::getStationStart, Matchers.is("C"))
+                .where(Trip::getStationEnd, Matchers.is("D"))
+                .where(Trip::getStartedJourneyAt, Matchers.is(3L))
+                .where(Trip::getCostInCents, Matchers.is(240))
+                .where(Trip::getZoneFrom, Matchers.is(2))
+                .where(Trip::getZoneTo, Matchers.is(2)));
+        Assert.assertThat(result.get(1).getTrips().get(1), IsPojo.pojo(Trip.class)
+                .where(Trip::getStationStart, Matchers.is("E"))
+                .where(Trip::getStationEnd, Matchers.is("F"))
+                .where(Trip::getStartedJourneyAt, Matchers.is(5L))
+                .where(Trip::getCostInCents, Matchers.is(200))
+                .where(Trip::getZoneFrom, Matchers.is(3))
+                .where(Trip::getZoneTo, Matchers.is(3)));
     }
 
 }
